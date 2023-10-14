fe421b7a8ad333e62e663ff79a164702526a1791
harivicky1010 <125113712+harivicky1010@users.noreply.github.com>
2023-10-14 10:26:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+var resume =[{
+ 
+    "name":"jhonny sins",
+    "work": "actor",
+    },
+    {
+    "qualification":"b.com",
+    "place": "america",
+    "age":"30",
+    },
+    {
+      "skills":" surfing , kayaking , sup,vollyball,cricket",
+      "experience": "6 years",
+      "languages" : "english ,tamil, hindi theriyathu poda",
+   
+   }]
+   
+   console.log(resume)
+   
+   
+   //JSON Over all loop ( for ,for in , for of, for each)
+   
